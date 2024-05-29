a9730c2736c936eca2f3f00b7b2e997ae53ca415
declanrjb <declanrjb@gmail.com>
2024-05-29 13:40:45 -0400

    test script

---
+++ arborist_main.js
@@ -0,0 +1,9 @@
+import Graph from "graphology";
+import Sigma from "sigma";
+
+const graph = new Graph();
+graph.addNode("1", { label: "Node 1", x: 0, y: 0, size: 10, color: "blue" });
+graph.addNode("2", { label: "Node 2", x: 1, y: 1, size: 20, color: "red" });
+graph.addEdge("1", "2", { size: 5, color: "purple" });
+
+const sigmaInstance = new Sigma(graph, document.getElementById("main-tree-container"));
